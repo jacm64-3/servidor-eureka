377079083e456dc67d8418cf75c21e07a923adbe
jose alberto castillo moctezuma <jacm64.3@gmail.com>
2023-10-25 16:49:24 -0600

    codigo inicial

---
+++ src/main/java/com/prueba/discovery/servidoreureka/ServidorEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.discovery.servidoreureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorEurekaApplication.class, args);
+	}
+
+}
